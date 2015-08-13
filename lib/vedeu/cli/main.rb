3de61015aa0d4883edee1a40fc11e43b711bfb12
Gavin Laking <gavinlaking@gmail.com>
2015-08-13 20:09:54 +0100

    Add more documentation.

--- lib/vedeu/cli/main.rb
+++ lib/vedeu/cli/main.rb
@@ -9,6 +9,13 @@ module Vedeu
     class Main < Thor
 
       desc 'new <name>', 'Create a skeleton Vedeu client application.'
+      long_desc <<-DESC
+        `vedeu new <name>` will create a new skeleton Vedeu client application.
+
+        Vedeu creates a new directory, <name>. If this directory already exists
+        Vedeu will only add files and subdirectories which are missing. It will
+        not overwrite existing files.
+      DESC
       # @param name [String]
       # @return [String]
       def new(name)
@@ -19,6 +26,16 @@ module Vedeu
 
       desc 'view <name>',
            'Create a new interface within the client application.'
+      long_desc <<-DESC
+        `vedeu view <name>` will create a new interface within the client
+        application.
+
+        Having run `vedeu new <name>` and changing into the newly created
+        client application directory, executing this command will create a new
+        view with associated files. If this view already exists Vedeu will only
+        add files and subdirectories which are missing. It will not overwrite
+        existing files.
+      DESC
       # @param name [String]
       # @return [String]
       def view(name)
